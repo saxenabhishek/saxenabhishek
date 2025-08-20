56f465f86bf16696d338e9f5eccfef7864c3954c
saxenabhishek <as7122000@gmail.com>
2025-08-20 05:02:13 -0400

    feat: new fonts

--- website/src/components/Education.jsx
+++ website/src/components/Education.jsx
@@ -3,12 +3,20 @@ import educationData from "../data/education.json";
 
 const Education = ({ expanded }) => (
   <div>
-    <h2 className="text-2xl font-light uppercase mb-2">Education</h2>
+    <h2 className="text-2xl leading-9 tracking-wide font-heading pb-4">
+      Education
+    </h2>
     {educationData.education.map((edu) => (
-      <div key={edu.institution} className=" mb-2">
-        <p className="text-lg ">{edu.institution}</p>
-        <p className="text-sm text-gray-400">{edu.degree}</p>
-        <p className="text-xs mt-1 text-gray-400 font-light">{edu.period}</p>
+      <div key={edu.institution} className="mb-2">
+        <p className="text-lg tracking-[0.1px] md:tracking-normal md:text-xl leading-7 pb-1">
+          {edu.institution}
+        </p>
+        <p className="text-sm md:text-base tracking-wide leading-4 md:leading-6 text-neutral-600 dark:text-neutral-400 pb-1">
+          {edu.degree}
+        </p>
+        <p className="text-xs md:text-sm leading-5 pb-4 text-neutral-600 dark:text-neutral-400">
+          {edu.period}
+        </p>
       </div>
     ))}
   </div>
